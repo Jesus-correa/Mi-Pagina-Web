25da03a59930d4241df5f59ba29c0ada6b5ecc23
Jesus-correa <jedacoma45@gmail.com>
2023-03-06 17:42:50 -0500

    Modificaciones en app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 const express = require ('express');
 const app = express();
 
+const path = require('path');
+
+const publicPath = path.resolve(__dirname,'../public');
+app.use(express.static(publicPath));
+
 const port = process.env.PORT || 3000
 app.listen(3000,() => {
     console.log("http://localhost:"+ port);

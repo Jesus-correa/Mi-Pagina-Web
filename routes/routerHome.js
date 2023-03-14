6f5e397deb882632fdf36291323f3e62fc661115
Jesus-correa <jedacoma45@gmail.com>
2023-03-13 21:32:17 -0500

    Se crea el archivo premium.html y se intenta vincular junto al gratuito.html al home.html... En teoria, la ruta funciona, pero sigue arrojando error

--- routes/routerHome.js
+++ routes/routerHome.js
@@ -6,7 +6,7 @@ router.get('/', (req, res) => {
 });
 
 router.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/home.html'))
+    res.sendFile(path.resolve(__dirname, './views/gratuito.html'))
 });
 
 module.exports = router;

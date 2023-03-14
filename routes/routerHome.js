d8dc970d19a42f36d868c6b095fb312def13870d
Jesus-correa <jedacoma45@gmail.com>
2023-03-13 21:14:18 -0500

    Modificaciones inconclusas en html y css de home

--- routes/routerHome.js
+++ routes/routerHome.js
@@ -2,8 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('ahora funciona?')
-    /* res.sendFile(path.resolve(__dirname, './views/home.html')) */
+    res.sendFile(path.resolve(__dirname, './views/home.html'))
+});
+
+router.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/home.html'))
 });
 
 module.exports = router;

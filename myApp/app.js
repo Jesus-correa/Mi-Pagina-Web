2dcbd0cab87f3b1ea86e931936a4320dbb660476
Jesus-correa <jedacoma45@gmail.com>
2023-03-15 19:38:52 -0500

    Recreo carpeta app.js

--- myApp/app.js
+++ myApp/app.js
@@ -1,10 +1,8 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 
-var routerHome = require(path.resolve(__dirname, './routes/routerHome'));
+
+var routerHome = require('./routes/routerHome.js');
 var routerFree = require('routerFree.js');
 var routerPremium = require('routerPremium.js');
 
@@ -23,21 +21,3 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routerHome);
 app.use('/free', routerFree);
 app.use('/premium', routerPremium)
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
